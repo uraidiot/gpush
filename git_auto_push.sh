0d6349abaffac45a50110dd09334dd543705c1fa
Uraidiot <454999803@qq.com>
2024-04-24 23:13:54 +0800

    feat(main): 优化分支跟踪

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e  # 任何命令的退出状态不为0时立即退出脚本
+#set -e # 任何命令的退出状态不为0时立即退出脚本
 
 # 获取当前分支
 current_branch=$(git symbolic-ref --short HEAD)
@@ -17,7 +17,7 @@ fi
 if [ -n "$(git status --porcelain)" ]; then
   echo "存在未提交或未跟踪文件，请先提交或添加文件"
   # 添加文件到暂存区
-  git add .  # 添加所有文件
+  git add . # 添加所有文件
   # 选择commit类型, 显示commit类型的中文名称
   read -p "请选择commit类型（feat、fix、docs、style、refactor、test、chore、init）:" commit_type
   # 提示输入commit信息
@@ -26,9 +26,25 @@ if [ -n "$(git status --porcelain)" ]; then
   git commit -m "$commit_type($current_branch): $commit_message"
 fi
 
-# 检查是否有远程分支和当前分支建立联系
-if [ -n "$(git config --get-regexp '^branch\.[^.]*\.remote$')" ]; then
-  echo "存在远程分支，正在尝试推送代码"
+# 获取当前分支的远程跟踪分支
+remote_branch=$(git rev-parse --abbrev-ref ${current_branch}@{upstream} 2>/dev/null)
+
+if [ -z "$remote_branch" ]; then
+  echo "当前分支 '$current_branch' 没有远程跟踪分支."
+  echo "尝试建立远程跟踪关系..."
+  # git branch --set-upstream-to=origin/$current_branch $current_branch
+  git push -u origin $current_branch
+  echo "建立远程跟踪关系成功"
+  # 提示执行推送操作
+  read -p "请确认是否要推送代码（y/n）:" confirm_push
+  if [ "$confirm_push" != "y" ]; then
+    echo "操作已取消"
+    exit 1
+  fi
+  # 上传代码到远程仓库
+  git push origin $current_branch
+else
+  echo "当前分支 '$current_branch' 与远程跟踪分支 '$remote_branch' 建立了联系."
   # 合并远程分支到本地分支变基
   git pull --rebase origin $current_branch
   # 检查是否有冲突
@@ -47,24 +63,4 @@ if [ -n "$(git config --get-regexp '^branch\.[^.]*\.remote$')" ]; then
   # 提示推送成功
   echo "推送成功"
   # 上传代码到远程仓库
-
-else
-  echo "当前分支没有远程分支，请先创建远程分支"
-  # 推送当前分支并建立与远程上游的跟踪关系
-  git push --set-upstream origin $current_branch
-  echo "建立远程跟踪关系成功"
-  # 提示执行推送操作
-  read -p "请确认是否要推送代码（y/n）:" confirm_push
-  if [ "$confirm_push" != "y" ]; then
-    echo "操作已取消"
-    exit 1
-  fi
-  # 上传代码到远程仓库
-  git push origin $current_branch
 fi
-
-
-
-
-
-
